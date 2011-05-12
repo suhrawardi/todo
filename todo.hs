f28aed3be6ceeede6e7bbe3a143664e009aadb4e
suhrawardi <suhrawardi@gmail.com>
2011-05-12 22:08:08 +0200

    At least we have a nice dummy package.
    Ignore-this: 6a98dbd32792abd90f0aa534994c1462
    
    darcs-hash:20110512200808-38dbf-6402551d083ba5b05096fc32fcdf5de350e0dd87.gz

--- todo.hs
+++ todo.hs
@@ -3,11 +3,26 @@
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
 --
 import System.Environment
+import Database.HDBC
+import Database.HDBC.Sqlite3
+
  
 -- | 'main' runs the main program
 main :: IO ()
 main = do s <- getArgs
-          putStrLn (haqify (head s))
+          if s == [] 
+            then putStrLn "empty!"
+            else case head s of
+                      "add"  -> addTask
+                      "list" -> putStrLn "Oh no!\nNothing there yet."
+                      _      -> putStrLn "Plainly wrong"
+
  
+addTask :: IO ()
+addTask = do putStrLn "Add a task:"
+             t <- getLine
+             putStrLn (haqify t)
+
+
 haqify :: String -> String
 haqify s = "Haq! " ++ s
